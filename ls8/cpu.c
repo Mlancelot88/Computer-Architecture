1dde5eba4e7ea253b521f9ef06b4e7b51ca9d317
Marious <mlancelot88@gmail.com>
2019-06-21 16:10:51 -0500

    Initial commits to cpu.c

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -1,67 +1,125 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "cpu.h"
 
 #define DATA_LEN 6
+#define SP 7
 
-/**
- * Load the binary bytes from a .ls8 source file into a RAM array
- */
-void cpu_load(struct cpu *cpu)
+unsigned char cpu_ram_read(struct cpu *cpu, unsigned char mar)
 {
-  char data[DATA_LEN] = {
-    // From print8.ls8
-    0b10000010, // LDI R0,8
-    0b00000000,
-    0b00001000,
-    0b01000111, // PRN R0
-    0b00000000,
-    0b00000001  // HLT
-  };
+  return cpu->ram[mar];
+}
 
+void cpu_ram_write(struct cpu *cpu, unsigned char index, unsigned char mdr)
+{
+  cpu->ram[index] = mdr;
+}
+
+void cpu_push(struct cpu *cpu, unsigned char val)
+{
+  cpu->registers[SP]--;
+  cpu_ram_written(cpu, cpu->registers[SP], val);
+}
+
+unsigned char cou_pop(struct cpu *cpu)
+{
+  unsigned char val = cpu_ram_read(cpu, cpu->registers[SP]);
+  cpu->registers[SP]++;
+  return val;
+}
+
+/* Load the binary bytes from a .ls8 source file into a RAM array */
+
+void cpu_load(struct cpu *cpu, char *filename)
+{
+  FILE *fp = fopen(filename, "r");
+  if (fp == NULL)
+  {
+    fprintf(stderr, "ls8: Error in opening file: %s\n", filename);
+    exit(2);
+  }
+
+  char line[8912];
   int address = 0;
 
-  for (int i = 0; i < DATA_LEN; i++) {
-    cpu->ram[address++] = data[i];
+  while (fgets(line, sizeof(line), fp) != NULL)
+  {
+    char *endptr;
+    unsigned char byte = strtoul(line, &endptr, 2);
+    if (endptr == line)
+    {
+      continue;
+    }
+    cpu->ram[address++] = byte;
   }
-
-  // TODO: Replace this with something less hard-coded
+  fclose(fp);
 }
 
-/**
- * ALU
- */
-void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB)
-{
-  switch (op) {
-    case ALU_MUL:
-      // TODO
-      break;
-
-    // TODO: implement more ALU ops
-  }
-}
-
-/**
- * Run the CPU
- */
+/** Run the CPU */
 void cpu_run(struct cpu *cpu)
 {
-  int running = 1; // True until we get a HLT instruction
+  //True until we get a HLT instruction
+  int running = 1;
+  unsigned char command, operand1, operand2;
 
-  while (running) {
-    // TODO
-    // 1. Get the value of the current instruction (in address PC).
-    // 2. Figure out how many operands this next instruction requires
-    // 3. Get the appropriate value(s) of the operands following this instruction
-    // 4. switch() over it to decide on a course of action.
-    // 5. Do whatever the instruction should do according to the spec.
-    // 6. Move the PC to the next instruction.
+  while (running)
+  {
+    command = cpu_ram_read(cpu, cpu->PC);
+    operand1 = cpu_ram_read(cpu, cpu->PC + 1);
+    operand2 = cpu_ram_read(cpu, cpu->PC + 2);
+
+    switch (command)
+    {
+    case LDI:
+      cpu->registers[operand1] = operand2;
+      cpu->PC += 3;
+      break;
+    case PRN:
+      printf("%d\n", cpu->registers[operand1]);
+      cpu->PC += 2;
+      break;
+    case MUL:
+      cpu->registers[operand1] *= operand2;
+      cpu->PC += 3;
+      break;
+    case ADD:
+      cpu->registers[operand1] += operand2;
+      cpu->PC += 3;
+      break;
+
+    cpu PUSH:
+      cpu_push(cpu, cpu->register[operand1]);
+      cpu->PC += 2;
+      break;
+
+    case POP:
+      cpu->registers[operand1] = cpu_pop(cpu);
+      cpu->PC += 2;
+      break;
+
+    case HLT:
+      running = 0;
+      cpu->PC += 1;
+      break;
+
+    default:
+    printf("Danger Will Robinson! DANGER!\n");
+    exit(1);
+    break;
+    }
   }
 }
 
-/**
- * Initialize a CPU struct
- */
+/** Initialize a COU struct */
 void cpu_init(struct cpu *cpu)
 {
-  // TODO: Initialize the PC and other special registers
+  for (int i = 0; i < 6; i++)
+  {
+    cpu->registers[i] = 0;
+  }
+
+  cpu->registers[7] = 0xF4;
+  cpu->PC = 0;
+  memset(cpu->ram, 0, sizeof(cpu->ram));
 }
